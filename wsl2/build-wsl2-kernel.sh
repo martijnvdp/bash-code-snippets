452226b218671bd3d9c6c2d6753134b13078d207
martijnvdp <m.vanderploeg@nl.ccv.eu>
2022-11-23 16:58:01 +0100

    path

--- wsl2/build-wsl2-kernel.sh
+++ wsl2/build-wsl2-kernel.sh
@@ -8,6 +8,7 @@ IMAGEFILE="bzImage-$(date +%s)"
 URL=$(curl https://api.github.com/repos/microsoft/WSL2-Linux-Kernel/releases/latest 2>/dev/null | jq -r '.tarball_url')
 USERDIR=$(wslpath "$(wslvar USERPROFILE)")
 TARGET="kernel"
+
 # check jq
 which jq || sudo apt install -y jq
 
@@ -33,14 +34,17 @@ sed -i 's/# CONFIG_NETFILTER_XT_TARGET_TPROXY is not set/CONFIG_NETFILTER_XT_TAR
 make -j${CPU} KCONFIG_CONFIG=Microsoft/config-wsl
 
 # copy kernel image to user dir/kernel
-mkdir ${USERDIR}/${TARGET}
+if [ ! -d "${USERDIR}/${TARGET}" ]; then
+    mkdir ${USERDIR}/${TARGET}
+fi
+
 cp arch/x86/boot/bzImage ${USERDIR}/${TARGET}/${IMAGEFILE}
 
 # create .wslconfig file and point kernel to newly build 
 readarray -d / -t userdirarr <<< "$USERDIR"
 cat << EOF >  ${USERDIR}/.wslconfig
 [wsl2]
-kernel=$(echo 'C:\Users\'$( echo ${userdirarr[4]})'\'${TARGET}'\'${IMAGEFILE})
+kernel=$(echo 'C:\\Users\\'$( echo ${userdirarr[4]})'\\'${TARGET}'\\'${IMAGEFILE})
 EOF
 
 # cleanup
@@ -52,3 +56,4 @@ read -p "Press a key to continue and restart WSL"
 cmd.exe /c "wsl --shutdown"
 
 # start wsl and check version with uname -r
+# and restart docker desktop
